cfc18475762b686c509848f2d964797e0481e15f
alisson98sb <alisson98sb@gmail.com>
2023-09-21 11:09:55 -0300

    First commit

---
+++ ignite-node/fundamentos-nodejs/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    const query = request.query;
+    console.log(query);
+    return response.json(["curso 1", "curso 2"]);
+})
+
+app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json(["curso 1", "curso 2", "curso adcionado"]);
+})
+
+app.put("/courses/:id", (request, response) => {
+    const params = request.params;
+    console.log(params);
+    return response.json(["Curso 6", "curso 2", "curso adcionado"]);
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json(["Curso 6", "curso 7", "curso 8"]);
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json(["Curso 6", "curso 7"]);
+})
+app.listen(3333);
