0a376cc41ced912d1b8a62d907bd063c46a9d6d7
alisson santana barbosa <alisson98sb@gmail.com>
2023-09-22 20:24:44 +0000

    create CategoriesRepository

---
+++ rentalx/src/model/Category.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidV4 } from "uuid";
+
+
+class Category {
+    id? : string;  //Como estamos usando a biblioteca uuid, salvaremos como string
+    name: string;
+    description: string;
+    created_at: Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuidV4();
+        }
+    }
+} 
+
+export { Category }
--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,16 +1,9 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 app.use(express.json());
 
-app.post("/courses", (request, response) => {
-    const body = request.body;
-    console.log(body);
-    return response.json({message: "ok"});
-})
-
-app.get("/", (request, response) => {
-    return response.json({message: "TESTE"})
-})
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running"));
